6f6834909e8e3183f53b5d02b714772b3323937f
marmai-jma <johane.marmai@gmail.com>
2019-12-07 11:28:27 +0100

    Modification de addCorsMappings dans LibraryApplication pour permettre la communication entre front et back et éviter code 403

--- src/main/java/com/bnpparibas/itg/mylibraries/libraries/LibrariesApplication.java
+++ src/main/java/com/bnpparibas/itg/mylibraries/libraries/LibrariesApplication.java
@@ -14,6 +14,7 @@ public class LibrariesApplication implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**");
+		registry.addMapping("/**")
+				.allowedMethods("GET", "PUT", "POST","DELETE");
 	}
 }
